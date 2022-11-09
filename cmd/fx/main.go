f21630985d6ba98cb302ba7ed24f6593b095d8be
Luis F F Spaniol <luis.spaniol@hurb.com>
2022-11-09 12:37:30 -0300

    fix FX

--- cmd/fx/main.go
+++ cmd/fx/main.go
@@ -5,12 +5,53 @@ import (
 	"dependencyInjection/Services/service1"
 	"dependencyInjection/Services/service2"
 	"fmt"
+	"html"
 	"net"
 	"net/http"
 
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
+
+type Handler2 struct {
+	remoteservice *service2.Service
+	log           *zap.Logger
+}
+
+// HandlewService1 is the handler for the service1 endpoint.
+func (h *Handler2) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.log.Info("Service2Handler.ServeHTTP")
+	h.remoteservice.Sleep()
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewHandlerForService2(remoteservice *service2.Service, zap *zap.Logger) *Handler2 {
+	return &Handler2{
+		remoteservice: remoteservice,
+		log:           zap,
+	}
+}
+
+type Handler1 struct {
+	remoteservice *service1.Service
+	log           *zap.Logger
+}
+
+// HandlewService1 is the handler for the service1 endpoint.
+func (h *Handler1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.log.Info("Service1Handler.ServeHTTP")
+	h.remoteservice.Sleep()
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewHandlerForService1(r *service1.Service, log *zap.Logger) *Handler1 {
+	return &Handler1{
+		remoteservice: r,
+		log:           log,
+	}
+}
 
 func NewServeMux(sevice1 *Handler1, sevice2 *Handler2) *http.ServeMux {
 	mux := http.NewServeMux()
